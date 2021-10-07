5d082e9d4883515b04c9f7ec0b7ad2996c0d118b
Masfikul Alam <masfikalamfrp@gmail.com>
2021-10-07 19:50:40 +0600

    font updated and build issue fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-serviceWorker.unregister();
